48b015d2c8a1e2dbbca5aca01170f9f2f167d666
joshgallantt <joshgallant78@gmail.com>
2022-08-14 15:11:24 +0100

    error message added

--- routes/logout.js
+++ routes/logout.js
@@ -4,6 +4,7 @@ const router = express.Router();
 router.post("/", function (req, res) {
   req.logout(function (err) {
     if (err) {
+      console.log("Error in Passport Logout Middleware");
       return next(err);
     }
     res.redirect("/login");
