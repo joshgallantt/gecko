1c4b27c3faa7c14d90e73cb59f4ca9d789972454
joshgallantt <joshgallant78@gmail.com>
2022-08-14 14:32:28 +0100

    fixed path refs

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.use("/api/project", isAuthenticated, project);
 
 // configure and start
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(__dirname + "client/build"));
+  app.use(express.static(__dirname + "/client/build"));
 }
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
